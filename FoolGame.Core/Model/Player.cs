9ebb4d4dd41ea8c66280c2104021acf07e2ead48
Алексей Яблонский <savageseed@yandex.ru>
2024-12-06 21:11:26 +0500

    Add Player model to FoolGame.Core
    Introduce the Player class with properties for Username and a collection of Cards. This addition allows for the management of player information and card assignment within the game.

---
+++ FoolGame.Core/Model/Player.cs
@@ -0,0 +1,22 @@
+﻿namespace FoolGame.Core.Model;
+
+public class Player
+{
+    private List<Card> _cards;
+    
+    public Player(string username)
+    {
+        Username = username;
+        _cards = [];
+    }
+    public string Username { get; }
+    public IEnumerable<Card> Cards => _cards;
+
+    public void AddCard(params Card[] cards)
+    {
+        foreach (var card in cards)
+        {
+            _cards.Add(card);
+        }
+    }
+}
