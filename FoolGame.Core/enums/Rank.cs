49ddab934f51f332b8376b24f407c99b3a1ec5a3
Алексей Яблонский <savageseed@yandex.ru>
2024-12-09 16:38:29 +0500

    Remove unused lower rank values from Rank enum

--- FoolGame.Core/enums/Rank.cs
+++ FoolGame.Core/enums/Rank.cs
@@ -8,10 +8,6 @@ namespace FoolGame.Core.enums
 {
     public enum Rank
     {
-        TWO = 2,
-        THREE = 3,
-        FOUR = 4,
-        FIVE = 5,
         SIX = 6,
         SEVEN = 7,
         EIGHT = 8,
